949a84e9f2a2747224211965f29652d23d61aab5
yuriy-dezhniuk <dezhniuk23@gmail.com>
2020-10-02 13:40:38 +0300

    responsive navbar part1

--- js/navbar.js
+++ js/navbar.js
@@ -1,14 +1,32 @@
 function nuvbarManipulation() {
-  let fullscreenSection = document.querySelector('.fullscreen');
-  let navbar = document.querySelector('.navbar');
+  let menuBtn = document.querySelector('.menu__btn');
 
   function changingNavbarPosition() {
+    let fullscreenSection = document.querySelector('.fullscreen');
+    let navbar = document.querySelector('.navbar');
+
     window.pageYOffset > fullscreenSection.scrollHeight ?
       navbar.style.position = 'fixed' : navbar.style.position = 'static';
   }
 
 
+  function openCloseMenu() {
+    // let menuVisibility = false;
+    let menuBox = document.querySelector('.menu__box');
+
+    menuBox.classList.toggle("close-menu");
+
+
+    // menuVisibility = menuVisibility === false ? true : false;
+
+    // console.log(menuVisibility);
+
+
+  }
+
+
     document.addEventListener('scroll', changingNavbarPosition, false);
+    menuBtn.addEventListener('click', openCloseMenu, false);
 }
 
 nuvbarManipulation();
