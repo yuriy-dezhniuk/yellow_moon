587dcfd2bcd1f91ada1e6ce1d044c055bd1c3e0b
yuriy-dezhniuk <dezhniuk23@gmail.com>
2020-11-19 21:43:58 +0200

    inspired block

--- js/navbar.js
+++ js/navbar.js
@@ -4,16 +4,20 @@ function nuvbarManipulation() {
   function changingNavbarPosition() {
     let fullscreenSection = document.querySelector('.fullscreen');
     let navbar = document.querySelector('.navbar');
+    let aboutUs = document.querySelector('.about-us');
 
-    window.pageYOffset > fullscreenSection.scrollHeight ?
-      navbar.style.position = 'fixed' : navbar.style.position = 'static';
+    if (window.pageYOffset > fullscreenSection.scrollHeight) {
+      navbar.style.position = 'fixed';
+      aboutUs.style.marginTop = `${navbar.scrollHeight}px`;
+    } else {
+      navbar.style.position = 'static';
+      aboutUs.style.marginTop = `${0}px`;
+    }
   }
 
-
   function openCloseMenu() {
-    // let menuVisibility = false;
     let menuBox = document.querySelector('.menu__box');
-
+    
     menuBox.classList.toggle("close-menu");
   }
 
